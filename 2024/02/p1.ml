956530691df0c62db0314e5850dde33cb05cf07c
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-12-02 14:40:28 +0100

    2024: day 02

---
+++ 2024/02/p1.ml
@@ -0,0 +1,38 @@
+let int_list_of_string str =
+  str |> String.split_on_char ' ' |> List.map int_of_string
+
+let rec parse ic =
+  try
+    let line = input_line ic in
+    int_list_of_string line :: parse ic
+  with End_of_file -> []
+
+let safe report =
+  let increasing =
+    match report with
+    | first :: second :: _ -> first < second
+    | _ -> failwith "safe"
+  in
+  let rec aux = function
+    | [] | _ :: [] -> true
+    | first :: second :: rest ->
+        let step = abs (second - first) in
+        let safe_step = 1 <= step && step <= 3
+        and safe_growth =
+          if increasing then first < second else first > second
+        in
+        safe_step && safe_growth && aux (second :: rest)
+  in
+  aux report
+
+let count target lst =
+  let rec aux count = function
+    | [] -> count
+    | head :: tail when head = target -> aux (count + 1) tail
+    | _ :: tail -> aux count tail
+  in
+  aux 0 lst
+
+let solve ic = ic |> parse |> List.map safe |> count true;;
+
+solve stdin |> Printf.printf "%d\n"
---
+++ 2024/02/p2.ml
@@ -0,0 +1,49 @@
+let int_list_of_string str =
+  str |> String.split_on_char ' ' |> List.map int_of_string
+
+let rec parse ic =
+  try
+    let line = input_line ic in
+    int_list_of_string line :: parse ic
+  with End_of_file -> []
+
+let safe report =
+  let increasing =
+    match report with
+    | first :: second :: _ -> first < second
+    | _ -> failwith "safe"
+  in
+  let rec aux = function
+    | [] | _ :: [] -> true
+    | first :: second :: rest ->
+        let step = abs (second - first) in
+        let safe_step = 1 <= step && step <= 3
+        and safe_growth =
+          if increasing then first < second else first > second
+        in
+        safe_step && safe_growth && aux (second :: rest)
+  in
+  aux report
+
+let rec exclude index = function
+  | [] -> failwith "exclude"
+  | _ :: tail when index = 0 -> tail
+  | head :: tail -> head :: exclude (index - 1) tail
+
+let rec any = function [] -> false | true :: _ -> true | _ :: tail -> any tail
+
+let safe report =
+  let safe_without index _ = safe (exclude index report) in
+  any (safe report :: List.mapi safe_without report)
+
+let count target lst =
+  let rec aux count = function
+    | [] -> count
+    | head :: tail when head = target -> aux (count + 1) tail
+    | _ :: tail -> aux count tail
+  in
+  aux 0 lst
+
+let solve ic = ic |> parse |> List.map safe |> count true;;
+
+solve stdin |> Printf.printf "%d\n"
