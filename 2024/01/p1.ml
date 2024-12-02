e5af579d9e61287c25da0325c3aeae4ef389aed2
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-12-02 12:40:52 +0100

    2024: day 01

---
+++ 2024/01/p1.ml
@@ -0,0 +1,22 @@
+let int_pair_of_string str =
+  let tokens = str |> String.split_on_char ' ' |> List.filter (( <> ) "") in
+  match tokens with
+  | [ first; second ] -> (int_of_string first, int_of_string second)
+  | _ -> failwith "int_pair_of_string"
+
+let rec parse ic =
+  try
+    let line = input_line ic in
+    int_pair_of_string line :: parse ic
+  with End_of_file -> []
+
+let distance (x, y) = abs (x - y)
+let sum = List.fold_left ( + ) 0
+
+let solve ic =
+  let left, right = parse ic |> List.split in
+  let left, right = (List.sort compare left, List.sort compare right) in
+  List.combine left right |> List.map distance |> sum
+;;
+
+solve stdin |> Printf.printf "%d\n"
